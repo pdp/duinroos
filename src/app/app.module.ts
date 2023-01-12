560dc9b3dfc75b17ff00c651ffe9c834055f0c37
Peter De Permentier <peter.depermentier@smals.be>
2023-01-12 19:29:41 +0100

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import {TabMenuModule} from "primeng/tabmenu";
+import {CarouselModule} from "primeng/carousel";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TabMenuModule,
+        CarouselModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
